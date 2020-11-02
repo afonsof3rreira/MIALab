a0a79b98f4b1af87cd18a75da98d0bdc84d08dd4
Oleksiy <ol.khoma@gmail.com>
2020-11-02 14:26:07 +0100

    Added HOG feature to preprocess parameters.

--- bin/main.py
+++ bin/main.py
@@ -61,7 +61,7 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
                           'registration_pre': True,
                           'coordinates_feature': True,
                           'intensity_feature': True,
-                          'gradient_intensity_feature': True
+                          'gradient_intensity_feature': True,
                           'HOG_feature': True}
 
     # load images for training and pre-process
