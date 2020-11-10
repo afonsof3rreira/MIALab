77d79b4ac14217b81502070edccdd9c2b7b62626
Afonso Ferreira <afonsof3rreira@users.noreply.github.com>
2020-11-10 17:57:47 +0100

    -comments removed

--- bin/Ubelix_file_reporter.py
+++ bin/Ubelix_file_reporter.py
@@ -28,21 +28,14 @@ def feature_writer(path: str, feature_dic: dict, filename: str):
     if not os.path.exists(os.path.join(path, filename + '.txt')):
 
         with open(os.path.join(path, filename + '.txt'), 'w') as outfile:
-            # I'm writing a header here just for the sake of readability
-            # Any line starting with "#" will be ignored by numpy.loadtxt
+
             outfile.write('-' * 37 + '\n')
             outfile.write('-' * 10 + ' Features tested ' + '-' * 10)
             outfile.write('\n' + '-' * 37)
             outfile.write('\n')
 
-            # Iterating through a ndimensional array produces slices along
-            # the last axis. This is equivalent to data[i,:,:] in this case
-            # for key, values in feature_dictionary.items():
-            #         print(key, " : ", str(values))
             for key, values in feature_dic.items():
-                # The formatting string indicates that I'm writing out
-                # the values in left-justified columns 7 characters in width
-                # with 2 decimal places.
+
                 if isinstance(values, list):
                     featureL = str()
                     outfile.write('\n' + '# 1st order features ')
