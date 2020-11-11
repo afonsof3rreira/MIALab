67a6ad6bd13f863a4799d766927b575485d1b187
Oleksiy <ol.khoma@gmail.com>
2020-11-11 19:51:14 +0100

    test 1: description: all features enabled, except all 1st order features
    # coordinates_feature
    # intensity_feature
    # gradient_intensity_feature

--- bin/main.py
+++ bin/main.py
@@ -65,9 +65,9 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
                           'normalization_pre': True,
                           'registration_pre': True,
                           'coordinates_feature': True,
-                          'intensity_feature': False,
-                          'gradient_intensity_feature': False,
-                          'first_order_feature': True
+                          'intensity_feature': True,
+                          'gradient_intensity_feature': True,
+                          'first_order_feature': False
                           # 'HOG_feature': False
                           }
 
