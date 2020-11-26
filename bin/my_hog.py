73675aae81cd75a58a85a7c403c71f35d4e3bc98
Afonso Ferreira <afonsof3rreira@users.noreply.github.com>
2020-11-26 09:56:36 +0100

    -A visualization draft was created (saves PNG images and a gif for a z brain slice)

--- bin/my_hog.py
+++ bin/my_hog.py
@@ -1,12 +1,14 @@
+import os
 import sys
 import time
-
+import shutil
 import SimpleITK as sitk
 import numpy as np
 from exercise.exercise_simpleitk import load_image
 import scipy.ndimage.filters as fltrs
 import matplotlib.pyplot as plt
 import pymia.filtering.filter as fltr
+import imageio
 
 
 class HOG_extractor(fltr.Filter):
@@ -29,8 +31,8 @@ class HOG_extractor(fltr.Filter):
         assert is_odd(block_size), "block size should be odd"
 
     def arr_convolver(self, img_arr, conv_mode='reflect'):
-        # padding image to include the starting and ending boundaries of the array
-        # convolution axis: z, y, x = 0, 1, 2
+        # padding the image to include the starting and ending boundaries of the array
+        # convolution axes: z, y, x = 0, 1, 2
         offset = self.block_size // 2
         pad_start = ((offset, 0), (offset, 0), (offset, 0))
         pad_end = ((0, offset), (0, offset), (0, offset))
@@ -46,21 +48,17 @@ class HOG_extractor(fltr.Filter):
         Args:
             image (sitk.Image): the image from which to apply HOG
 
-
         Returns:
             np.array: A vector containing the HOG-features
             :param image:
             :param params:
         """
         img_arr = sitk.GetArrayFromImage(image)
-        print('orginal image size = ' + str(img_arr.shape))
-        or_z, or_y, or_x = img_arr.shape
-
+        print('original image size = ' + str(img_arr.shape))
 
         #   creating the output image
         img_out = sitk.Image(image.GetSize(), sitk.sitkVectorFloat32, self.theta_bins * self.phi_bins)
         img_out_arr = sitk.GetArrayFromImage(img_out)
-        # print(img_out.GetNumberOfComponentsPerPixel())
 
         #   calculating image 3D gradient
         self.arr_convolver(img_arr)
@@ -69,20 +67,18 @@ class HOG_extractor(fltr.Filter):
         blocks_zyx = nr_of_blocks(img_arr.shape, self.block_size, self.block_displacement)
 
         # 2D image test (to be removed)
-        img_test_1 = sitk.Image(image.GetSize(), sitk.sitkVectorFloat32, 1)
+        img_test_1 = sitk.Image(image.GetSize(), sitk.sitkVectorFloat32, self.theta_bins * self.phi_bins)
         img_test_1_arr = sitk.GetArrayFromImage(img_test_1)
-        # print(img_test_1_arr.shape)
-        cord_z = None
 
         #   cord_z, cord_y, cord_x = are the starting-window-coordinates of the padded image
         # and correspond to the central window coordinates on the original image
-        print('-' * 5 + str(blocks_zyx[1]) + ' blocks to process' + 5 * '-')
-        for z in range(7, 8):  # range(blocks_zyx[0):
-
+        print('.' * 5 + ' processing brain slice z = ' + str(int(7 * self.block_displacement)))
+        print('      (' + str(blocks_zyx[1]) + ' blocks to process)')
+        slice_val = 105
+        for z in range(slice_val, slice_val + 1):  # range(blocks_zyx[0):
             cord_z = int(self.block_displacement * z)
 
             for y in range(blocks_zyx[1]):
-
                 cord_y = int(self.block_displacement * y)
                 start = time.perf_counter()
 
@@ -92,7 +88,7 @@ class HOG_extractor(fltr.Filter):
                     img_out_arr[cord_z, cord_y, cord_x] = self.neighborhood_hog_extractor(cord_z, cord_y, cord_x)
 
                     # 2D image test (to be removed), z ~ 100  slice
-                    img_test_1_arr[cord_z, cord_y, cord_x] = img_out_arr[cord_z, cord_y, cord_x][22]
+                    img_test_1_arr[cord_z, cord_y, cord_x] = img_out_arr[cord_z, cord_y, cord_x]
                     # print(img_test_1_arr[cord_z, cord_y, cord_x])
 
                 end = time.perf_counter()
@@ -100,9 +96,13 @@ class HOG_extractor(fltr.Filter):
             break
 
         # 2D image test (to be removed), z ~ 100  slice
-        plt.figure()
-        plt.imshow(img_test_1_arr[int(7 * self.block_displacement)])
-        plt.show()
+        slice_z = int(slice_val * self.block_displacement)
+        img_slice = sitk.GetImageFromArray(img_test_1_arr[int(slice_val * self.block_displacement)])
+        main_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mia-result/HOG_testing')
+        dir_path = os.path.join(main_path, 'size_' + str(self.block_size) + '_disp_' + str(self.block_displacement))
+        full_path = os.path.join(dir_path, 'brain_slice_' + str(slice_z) + '_.nii')
+        sitk.WriteImage(img_slice, full_path)
+        print('HOG extraction finished')
 
         img_out = sitk.GetImageFromArray(img_out_arr)
         img_out.CopyInformation(image)
@@ -245,7 +245,6 @@ class HOG_extractor(fltr.Filter):
 
 
 def nr_of_blocks(img_arr_shape, block_size, block_displacement):
-
     #   Calculating nr of blocks from which to extract image histograms
     # maximal coordinates that can be reached (inclusive)
     or_z = img_arr_shape[0] - 1
@@ -299,28 +298,50 @@ def is_odd(nr):
         return True
 
 
-#   testing the algortihm
+# loading the image
 path1 = 'C:/Users/afons/PycharmProjects/MIAlab project/data/train/116524/T1native.nii.gz'
 image1 = load_image(path1, False)
-hog_example = HOG_extractor()
-hog_example.execute(image1)
 
-#   testing new function
-# np.random.seed(1)
-# matrix = np.random.rand(200, 100, 300)
-# matrix = np.multiply(matrix, 10)
-# matrix = np.floor(matrix)
-# matrix = matrix.astype(int)
-# print(matrix.shape)
-# block_size = 5
-# pad_val = block_size // 2
-# # block size would be 5 -> pad 2 each side
-# pad = ((pad_val, 0), (pad_val, 0), (pad_val, 0))
-# new_mat = np.pad(matrix, pad, 'symmetric')
+# ----- testing the algorithm -----
+block_size = 15
+block_displacement = 1
+
+# creating a sub-directory for the parameters used
+main_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mia-result/HOG_testing')
+new_dir_path = os.path.join(main_path, 'size_' + str(block_size) + '_disp_' + str(block_displacement))
+
+# -----------------------comment / uncomment------------------
+# # this deletes the dir if exists
+# if os.path.exists(new_dir_path):
+#     shutil.rmtree(new_dir_path)
+# os.makedirs(new_dir_path)
 #
-# pad2 = ((0, pad_val), (0, pad_val), (0, pad_val))
-# new_mat2 = np.pad(new_mat, pad2, 'symmetric')
+# hog_example = HOG_extractor(block_size=block_size, block_displacement=block_displacement)
+# hog_example.execute(image1)
+# ------------------------comment / uncomment-----------------
 #
-# print(new_mat2.shape)
-# print('---')
-# nr_of_blocks(matrix.shape, block_size, 1)
+# getting slice 100
+image1_arr = sitk.GetArrayFromImage(image1)
+image1_arr_22 = image1_arr[105]
+image1_22 = sitk.GetImageFromArray(image1_arr_22)
+
+# loading brain slice image
+image = sitk.ReadImage(os.path.join(new_dir_path, 'brain_slice_105_.nii'))
+image_arr = sitk.GetArrayFromImage(image)
+print(image_arr.shape)
+
+# saving every feature image
+file_names = []
+for feature_i in range(image_arr.shape[2]):
+    saving_img_arr = image_arr[:, :, feature_i]
+    saving_img = sitk.GetImageFromArray(saving_img_arr)
+    file_name = 'feature{0}.png'.format(feature_i)
+    saving_path = os.path.join(new_dir_path, file_name)
+    sitk.WriteImage(sitk.Cast(sitk.RescaleIntensity(saving_img), sitk.sitkUInt8), saving_path)
+    file_names.append(file_name)
+
+# creating a gif
+images = []
+for filename in file_names:
+    images.append(imageio.imread(os.path.join(new_dir_path, filename)))
+imageio.mimsave(os.path.join(new_dir_path, 'video.gif'), images)
