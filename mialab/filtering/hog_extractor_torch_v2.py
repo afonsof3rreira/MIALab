9cf338e53573378557284720672252807c4861b5
Afonso Ferreira <afonsof3rreira@users.noreply.github.com>
2020-12-13 14:18:00 +0100

    - debugging prints added

--- mialab/filtering/hog_extractor_torch_v2.py
+++ mialab/filtering/hog_extractor_torch_v2.py
@@ -363,6 +363,16 @@ class SimpleHOGModule(nn.Module):
             del phi
             del theta
             torch.cuda.empty_cache()
+            t = torch.cuda.get_device_properties(0).total_memory
+            c = torch.cuda.memory_cached(0)
+            a = torch.cuda.memory_allocated(0)
+            f = c - a  # free inside cache
+            print('='*10)
+            print(t)
+            print(c)
+            print(a)
+            print(f)
+            print('='*10)
 
             # print(low_p_ordered_by_low_t.size())
 
